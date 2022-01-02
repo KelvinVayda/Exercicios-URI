523d0ad22aaf7af0a986e58556c41ff4330bad14
KelvinVayda <kelvinsoares_23@hotmail.com>
2022-01-02 12:48:09 -0300

    Atz 2.0

---
+++ URI/URI_1037/URI_1037/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+namespace URI_1037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            if (n1 < 0.0 || n1 > 100.0)
+            {
+                Console.WriteLine("Fora de intervalo");
+            }
+            else if (n1 <= 25.0)
+            {
+                Console.WriteLine("Intervalo [0,25]");
+            }
+            else if (n1 <= 50.0)
+            {
+                Console.WriteLine("Intervalo (25,50]");
+            }
+            else if (n1 <= 75.0)
+            {
+                Console.WriteLine("Intervalo (50,75]");
+            }
+            else
+            {
+                Console.WriteLine("Intervalo (75,100]");
+            }
+        }
+    }
+}
---
+++ URI/URI_1041/URI_1041/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+namespace URI_1041
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y
+        }
+    }
+}
