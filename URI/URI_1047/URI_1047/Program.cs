b9b8d2c130f2f237368ee2f7732405947d0f4d04
KelvinVayda <kelvinsoares_23@hotmail.com>
2022-01-04 12:25:23 -0300

    Atz 2.2

---
+++ URI/URI_1047/URI_1047/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace URI_1047
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int horasInicio, minutosInicio, horasFim, minutosFim, h, m, inicio, fim, duracao;
+
+            String[] valores = Console.ReadLine().Split(' ');
+            horasInicio = int.Parse(valores[0]);
+            minutosInicio = int.Parse(valores[1]);
+            horasFim = int.Parse(valores[2]);
+            minutosFim = int.Parse(valores[3]);
+
+            inicio = horasInicio * 60 + minutosInicio;
+            fim = horasFim * 60 + minutosFim;
+
+            if (inicio < fim)
+            {
+                duracao = fim - inicio;
+            }
+            else
+            {
+                duracao = (24 * 60 - inicio) + fim;
+            }
+
+            h = duracao / 60;
+            m = duracao % 60;
+
+            Console.WriteLine("O JOGO DUROU " + h + " HORA(S) E " + m + " MINUTO(S)");
+
+
+
+        }
+    }
+}
---
+++ URI/URI_1049/URI_1049/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace URI_1049
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string coluna = Console.ReadLine();
+            string especie = Console.ReadLine();
+            string tipo = Console.ReadLine();
+
+            if (coluna == "vertebrado")
+            {
+                if (especie == "ave")
+                {
+                    if (tipo == "carnivoro")
+                    {
+                        Console.WriteLine("aguia");
+                    }
+                    else
+                    {
+                        Console.WriteLine("pomba");
+                    }
+                }
+                else
+                {
+
+                    if (tipo == "onivoro")
+                        Console.WriteLine("homem");
+
+                    else
+                    {
+                        Console.WriteLine("vaca");
+                    }
+                }
+            }
+            else
+            {
+                if (especie == "inseto")
+                {
+                    if (tipo == "hematofago")
+                    {
+                        Console.WriteLine("pulga");
+                    }
+                    else
+                    {
+                        Console.WriteLine("lagarta");
+                    }
+                }
+                else
+                {
+                    if (tipo == "hematofago")
+                    {
+                        Console.WriteLine("sanguessuga");
+                    }
+                    else
+                    {
+                        Console.WriteLine("minhoca");
+                    }
+                }
+            }
+        }
+    }
+}
+
+
