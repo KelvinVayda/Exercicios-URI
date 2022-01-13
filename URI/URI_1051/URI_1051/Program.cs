082343abdf7ad8dab53a5f8fe2a051566cc311a5
KelvinVayda <kelvinsoares_23@hotmail.com>
2022-01-13 12:34:06 -0300

    Exe_1051

---
+++ URI/URI_1051/URI_1051/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+namespace URI_1051
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double renda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double imposto;
+            if (renda <= 2000.00) {
+                imposto = 0.0;
+            }
+            else if (renda <= 3000.00) {
+                imposto = (renda - 2000.00) * 0.08;
+            }
+            else if (renda <= 4500.00) {
+                imposto = (renda - 3000.00) * 0.18 + 1000.0 * 0.08;
+            }
+            else
+            {
+                imposto = (renda - 4500.00) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+            }
+            if ( imposto == 0.0) {
+                Console.WriteLine("Isento");
+            }
+            else
+            {
+                Console.WriteLine("R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
