1f3f7361f207d8450c9575fd9e6e96e03d3cbf7e
KelvinVayda <kelvinsoares_23@hotmail.com>
2022-01-15 13:53:07 -0300

    Exe_1059

---
+++ URI/URI_1059/URI_1059/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace URI_1059
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 2;
+
+            while (n <= 100 )
+            {
+                if (n % 2 == 0)
+                {                    
+                    Console.WriteLine(n);
+                    n = n + 2 ;
+                }
+            }
+        }
+    }
+}
