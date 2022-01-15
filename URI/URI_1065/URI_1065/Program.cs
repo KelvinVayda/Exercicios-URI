e9c728494031ebcb0ab050a2f9f31991410d17bc
KelvinVayda <kelvinsoares_23@hotmail.com>
2022-01-15 13:15:00 -0300

    Exe_1065

---
+++ URI/URI_1065/URI_1065/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace URI_1065
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, n4, n5, par;
+
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            n3 = int.Parse(Console.ReadLine());
+            n4 = int.Parse(Console.ReadLine());
+            n5 = int.Parse(Console.ReadLine());
+            par = 0;
+
+            if (n1 % 2 == 0)
+            {
+                par = par + 1;
+            }
+            if (n2 % 2 == 0)
+            {
+                par = par + 1;
+            }
+            if (n3 % 2 == 0)
+            {
+                par = par + 1;
+            }
+            if (n4 % 2 == 0)
+            {
+                par = par + 1;
+            }
+            if (n5 % 2 == 0)
+            {
+                par = par + 1;
+            }
+            Console.WriteLine(par + " valores pares");
+        }
+    }
+}
