828d028b867ca4cd7cd36078b4d2022d4fa207e9
KelvinVayda <kelvinsoares_23@hotmail.com>
2022-01-15 12:56:09 -0300

    Atz 2.4

---
+++ URI/URI_1060/URI_1060/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+namespace URI_1060
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3, n4, n5, n6, soma;
+            n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n4 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n5 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n6 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            soma = 0.0;
+
+            if (n1 > 0)
+            {
+                soma = soma + 1;
+            }
+            if (n2 > 0)
+            { 
+                soma = soma + 1;
+            }
+            if (n3 > 0)
+            {
+                soma = soma + 1;
+            }
+            if (n4 > 0)
+            {
+                soma = soma + 1;
+            }
+            if (n5 > 0)
+            {
+                soma = soma + 1;
+            }
+            if (n6 > 0)
+            {
+                soma = soma + 1;
+            }
+            Console.WriteLine(soma + " valores positivos");
+        }
+    }
+}
+
---
+++ URI/URI_1064/URI_1064/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1064
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3, n4, n5, n6, soma;
+            n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n4 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n5 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n6 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            soma = 0.0;
+            int cont = 0;
+
+            if (n1 > 0.0)
+            {
+                cont = cont + 1;
+                soma = soma + n1;
+            }
+            if (n2 > 0.0)
+            {
+                cont = cont + 1;
+                soma = soma + n2;
+            }
+            if (n3 > 0.0)
+            {
+                cont = cont + 1;
+                soma = soma + n3;
+            }
+            if (n4 > 0.0)
+            {
+                cont = cont + 1;
+                soma = soma + n4;
+            }
+            if (n5 > 0.0)
+            {
+                cont = cont + 1;
+                soma = soma + n5;
+            }
+            if (n6 > 0)
+            {
+                cont = cont + 1;
+                soma = soma + n6;
+            }
+            Console.WriteLine(cont + " valores positivos");
+            double media = soma / cont ;
+            Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
+
+       
+    
+
