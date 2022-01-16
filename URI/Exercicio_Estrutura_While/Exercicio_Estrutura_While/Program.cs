d57960339df91acfbe8b982ca5aefe09ea5b772b
KelvinVayda <kelvinsoares_23@hotmail.com>
2022-01-16 12:32:04 -0300

    Exe_Estrutura_While_2

---
+++ URI/Exercicio_Estrutura_While/Exercicio_Estrutura_While/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio_Estrutura_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, soma, media;
+            int cont = 0;
+            x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            soma = 0.0;
+            while (x >= 0)
+            {
+                soma = soma + x;
+                cont++;
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            }
+            if (cont == 0)
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+            else
+            {
+                media = soma / cont;
+                Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+        }
+
+    }
+}
