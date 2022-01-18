461ff929b3e4af8b848333dc670df2b31751fd6c
KelvinVayda <kelvinsoares_23@hotmail.com>
2022-01-18 10:38:51 -0300

    Exe_1114

---
+++ URI/URI_1114/URI_1114/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace URI_1114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            while (x != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                x = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido");
+
+        }
+    }
+}
