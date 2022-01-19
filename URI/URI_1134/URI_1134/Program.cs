47abacbeac7bc02aa9fa09f28bb9bb2edfc8ed43
KelvinVayda <kelvinsoares_23@hotmail.com>
2022-01-19 16:09:05 -0300

    Exe_1134_While

---
+++ URI/URI_1134/URI_1134/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace URI_1134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Al = 0;
+            int Gas = 0;
+            int Dis = 0;
+            int comb = int.Parse(Console.ReadLine());
+
+            while (comb != 4)
+            {
+                if (comb == 1)
+                {
+                    Al++;
+                }
+                else if (comb == 2)
+                {
+                    Gas++;
+                }
+                else if (comb == 3)
+                {
+                    Dis++;
+                }
+                comb = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: " + Al);
+            Console.WriteLine("Gasolina: " + Gas);
+            Console.WriteLine("Diesel: " + Dis);
+        }
+    }
+}
+
