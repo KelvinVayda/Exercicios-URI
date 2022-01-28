4167b56341341fa95065c9ba814b926c6994d8b1
KelvinVayda <kelvinsoares_23@hotmail.com>
2022-01-28 08:52:51 -0300

    Exe_1154_1159_while

---
+++ URI/URI_1154/URI_1154/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+namespace URI_1154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = int.Parse(Console.ReadLine());       //Entrada de Numeros Indeterminados//
+            int soma = 0;
+            int cont = 0;
+
+            while (idade >= 0) //Enquanto idade for maior igual a 0, ou seja numeros positivos//
+            {
+                soma += idade; // Soma+idade
+                cont++;        //cont +1
+                idade = int.Parse(Console.ReadLine());
+            }
+            double media = (double)soma / cont;
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ URI/URI_1159/URI_1159/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace URI_1159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            while (x != 0)
+            {   // Se for impar, some mais 1
+                if (x % 2 != 0)
+                {
+                    x++;
+                }
+                // expressao simplificada: 5 * x + 20
+                int soma = x + x + 2 + x + 4 + x + 6 + x + 8;
+                Console.WriteLine(soma);
+
+                x = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
